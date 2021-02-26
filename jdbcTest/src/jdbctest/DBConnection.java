fe1a68a947138a6d2faf09cec85016d100dfb9e7
mika <73530023+2everlove@users.noreply.github.com>
2021-02-26 12:36:06 +0900

    JSP:db connect in java
    Connection
    DriverManager
    PreparedStatement
    ResultSet

---
+++ jdbcTest/src/jdbctest/DBConnection.java
@@ -0,0 +1,53 @@
+package jdbctest;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
+public class DBConnection {
+	private static String driverClass = "oracle.jdbc.OracleDriver";		// oracle 주소
+	private static String url = "jdbc:oracle:thin:@localhost:1521:xe"; // db 주소 thin(drive type) xe버전
+	private static String username = "HR"; //사용자 (DB)
+	private static String password = "1234"; //비밀번호
+	private static Connection conn = null;
+	private static PreparedStatement pstmt = null;
+			
+	public static void main(String[] args) {
+		String sql = "INSERT INTO t_student VALUES (109,'유유')"; //sql 문법
+		try {
+			Class.forName(driverClass); //driver loading
+			conn = DriverManager.getConnection(url, username, password);
+			System.out.println("DB 연결 성공 ("+conn+")");
+			
+			
+			
+			//학생 추가
+			pstmt = conn.prepareStatement(sql); //sql 처리할 객체 생성
+			pstmt.executeUpdate();
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			
+			int idx = sql.indexOf("(");
+			String value = sql.substring(idx);
+			System.out.println("추가 완료 "+value);
+			
+			if(pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if(conn != null) {
+				try {
+					conn.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
---
+++ jdbcTest/src/jdbctest/DBSelect.java
@@ -0,0 +1,56 @@
+package jdbctest;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class DBSelect {
+	private static String driverClass = "oracle.jdbc.OracleDriver";		// oracle 주소
+	private static String url = "jdbc:oracle:thin:@localhost:1521:xe"; // db 주소 thin(drive type) xe버전
+	private static String username = "HR"; //사용자 (DB)
+	private static String password = "1234"; //비밀번호
+	private static Connection conn = null;
+	private static PreparedStatement pstmt = null;
+	private static ResultSet rs = null;
+	
+			
+	public static void main(String[] args) {
+		try {
+			Class.forName(driverClass); //driver loading
+			conn = DriverManager.getConnection(url, username, password);
+			System.out.println("DB 연결 성공 ("+conn+")");
+			
+			//학생 목록 조회
+			String sql = "SELECT * FROM t_student";
+			pstmt = conn.prepareStatement(sql);
+			rs = pstmt.executeQuery(); //db의 반환값을 가져욤
+			int i=0;
+			while(rs.next()) { //자료가 있으면 출력
+				System.out.println(i+"번 인덱스");
+				System.out.print("학번 : "+rs.getInt("studentid"));
+				System.out.println(", 이름 : "+rs.getString("name"));
+				i++;
+			}
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			if(pstmt != null) {
+				try {
+					pstmt.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+			if(conn != null) {
+				try {
+					conn.close();
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
